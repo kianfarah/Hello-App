2b1f2f96e8a14e0f82686f457803c1c9d091efab
Isak Farah <dado.org@hotmail.com>
2026-04-22 16:18:07 +0200

    second commit

--- src/main/java/com/farah/helloapp/UserRepository.java
+++ src/main/java/com/farah/helloapp/UserRepository.java
@@ -2,6 +2,9 @@ package com.farah.helloapp;
 
 import com.farah.helloapp.entities.User;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends ListCrudRepository<User, Long> {
 }
+
